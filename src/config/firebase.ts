aa929b47127ef6aaa71a640b731433e15b51c0ee
pavitersingh88 <104886026+pavitersingh88@users.noreply.github.com>
2025-01-25 20:13:16 -0500

    Update firebase.ts
    Remove api keys

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -5,13 +5,13 @@ import { getStorage } from 'firebase/storage';
 import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBxX-ELGic95zBISISI46VZ0kjfrhW00VI",
-  authDomain: "sherlife-13fda.firebaseapp.com",
-  projectId: "sherlife-13fda",
-  storageBucket: "sherlife-13fda.appspot.com",
-  messagingSenderId: "381986274509",
-  appId: "1:381986274509:web:015265105bff482f70bd84",
-  measurementId: "G-30YLRR3XTS"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
